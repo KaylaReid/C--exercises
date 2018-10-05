8c6b4ce4186561316f82ad2badbbe51ccaf10ef5
Kayla Reid <iamkaylareid@gmail.com>
2018-10-05 11:50:58 -0500

    started exercise

--- classes/Program.cs
+++ classes/Program.cs
@@ -1,32 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace classes
+namespace Company // the namespace conects the files that all use this name
 {
-    public class Company
-{
+    // blue print, all empolyeess will be build in this way
+    public class Employee {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+    public class Company {
 
-    // Some readonly properties (let's talk about gets, baby)
-    public string Name { get; }
-    public DateTime CreatedOn { get; }
+        // Create a constructor method that accepts two arguments:
+        //     1. The name of the company
+        //     2. The date it was created
+        // The constructor will set the value of the public properties
 
-    // Create a public property for holding a list of current employees
+        // method (function) to build new instances of a company 
+        public Company(string CompanyName, DateTime CompanyCreatedOn){
+            Name = CompanyName;
+            CreatedOn = CompanyCreatedOn;
+        }
 
-    /*
-        Create a constructor method that accepts two arguments:
-            1. The name of the company
-            2. The date it was created
+        // blue prints for a company 
+        public string Name { get; } 
+        public DateTime CreatedOn { get; } 
 
-        The constructor will set the value of the public properties
-    */
-}
+        // Create a public property for holding a list of current employees
+        public List<Employee> employees = new List<Employee>();
+        
+    }
 
     class Program
 {
     static void Main(string[] args)
     {
         // Create an instance of a company. Name it whatever you like.
+        Company Apple = new Company ("Apple", DateTime.Now);
 
         // Create three employees
+        Employee Steve = new Employee ();
+        Steve.FirstName = "Steve";
+        Steve.LastName = "Brownlee";
+        Steve.Title = "fish manager";
+        Steve.StartDate = new DateTime (2012, 12, 19);
+
+        Employee Meg = new Employee ();
+        Meg.FirstName = "Meg";
+        Meg.LastName = "Ducharme";
+        Meg.Title = "Leader";
+        Meg.StartDate = new DateTime (2009, 11, 30);
+
+        Employee Kimmy = new Employee ();
+        Kimmy.FirstName = "Kimmy";
+        Kimmy.LastName = "Bird";
+        Kimmy.Title = "New Awesome TA";
+        Kimmy.StartDate = new DateTime (2016, 3, 7);
 
         // Assign the employees to the company
 
