117443581dce5373bbf8c7864b0de03b8ed9bc9f
Kayla Reid <iamkaylareid@gmail.com>
2018-10-10 15:40:18 -0500

    finished 2

--- linq/Program.cs
+++ linq/Program.cs
@@ -11,12 +11,34 @@ namespace linq
             // Find the words in the collection that start with the letter 'L'
             List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
 
+            // IEnumerable<string> LFruits = from fruit in fruits
+            // where fruit.StartsWith("L")
+            // select fruit;
+            // foreach (string LF in LFruits)
+            // {
+            //     Console.WriteLine($"{LF}");
+            // }
+
             IEnumerable<string> LFruits = from fruit in fruits
-            where fruit.StartsWith("L")
+            where fruit[0] == 'L'
             select fruit;
             foreach (string LF in LFruits)
             {
                 Console.WriteLine($"{LF}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+            {
+                foreach (int n in fourSixMultiples)
+                {
+                    Console.WriteLine($"{n}");
+                }
             }
         }
     
