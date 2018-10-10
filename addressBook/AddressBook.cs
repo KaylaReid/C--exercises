0a527ededdc925a2c7e9045bce06c91076a67076
Kayla Reid <iamkaylareid@gmail.com>
2018-10-10 09:55:12 -0500

    making progress

---
+++ addressBook/AddressBook.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace addressBook
+{
+    public class AddressBook 
+    {
+        // dict of contacts
+        Dictionary<Contact, email> AddressBookContact = new Dictionary<Contact, email>();
+
+
+        // two methods 
+        // AddContact
+        // GetByEmail
+    }
+}
