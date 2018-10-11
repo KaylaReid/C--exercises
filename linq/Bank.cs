36ababd2cb03ca54fb02113a5c0dada7d12b9f52
Kayla Reid <iamkaylareid@gmail.com>
2018-10-11 16:30:01 -0500

    almost done

---
+++ linq/Bank.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
+}
--- linq/Program.cs
+++ linq/Program.cs
@@ -97,6 +97,121 @@ namespace linq
 
             Console.WriteLine($"The most expensive product is {prices.Max()}");
 
+            /*
+            Store each number in the following List until a perfect square
+            is detected.
+
+            Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+                */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            List<int> nonSquare = wheresSquaredo.TakeWhile(n => Math.Sqrt(n) % 1 != 0).ToList();
+            foreach (int n in nonSquare)
+            {
+                Console.WriteLine($"List of sqrt {n}");
+            }
+
+            /*
+                TASK:
+                As in the previous exercise, you're going to output the millionaires,
+                but you will also display the full name of the bank. You also need
+                to sort the millionaires' names, ascending by their LAST name.
+
+                Example output:
+                    Tina Fey at Citibank
+                    Joe Landy at Wells Fargo
+                    Sarah Ng at First Tennessee
+                    Les Paul at Wells Fargo
+                    Peg Vale at Bank of America
+            */
+
+
+
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
+                new Bank(){ Name="First Tennessee", Symbol="FTB"},
+                new Bank(){ Name="Wells Fargo", Symbol="WF"},
+                new Bank(){ Name="Bank of America", Symbol="BOA"},
+                new Bank(){ Name="Citibank", Symbol="CITI"},
+            };
+
+            // Build a collection of customers who are millionaires
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            /*
+                You will need to use the `Where()`
+                and `Select()` methods to generate
+                instances of the following class.
+
+                public class ReportItem
+                {
+                    public string CustomerName { get; set; }
+                    public string BankName { get; set; }
+                }
+            */
+            List<Customer> millionaireReport = customers.Where(c => c.Balance >= 1_000_000)
+                .Select(c => new Customer()
+                {
+                    Name = c.Name,
+                    Bank = banks.Find(BankId => BankId.Symbol == c.Bank).Name,
+                    Balance = c.Balance
+                }).ToList();
+
+            foreach (Customer customer in millionaireReport)
+            {
+                Console.WriteLine($"{customer.Name} at {customer.Bank}");
+            }
+
+
+
+
+
+            // first step
+            // var millionaires = customers.Where(c => c.Balance >= 1_000_000);
+
+            // foreach(Customer MC in millionaires)
+            // {
+            //     Console.WriteLine($"Mil customer {MC.Name} {MC.Balance}");
+            // }
+
+            /*
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
+
+            // second step
+            // var millionaires = from mill in customers
+            // where mill.Balance >= 1_000_000
+            // group mill by mill.Bank into MCByBank
+            // select new {
+            //     MillCustomersBank = MCByBank.Key,
+            //     MillCustomers = MCByBank.ToList()
+            // };
+
+            // foreach (var MC in millionaires)
+            // {
+            //     Console.WriteLine($"this them {MC.MillCustomersBank} {MC.MillCustomers.Count()}");
+            // }
         }
     }
 }
